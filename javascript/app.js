b29494ea8fa3403039ea39a0d697396f3d125c11
John Contreras <contrerasnet@gmail.com>
2012-08-28 12:17:40 -0500

    initial commit

---
+++ javascript/app.js
@@ -0,0 +1,118 @@
+// load in order of
+// models
+// collections
+// views
+// routers
+// app
+
+var app = app || {};
+var ENTER_KEY = 13;
+
+// ***************************//
+// applicatoin models
+// ***************************//
+
+$(function( $ ){
+  'use strict';
+
+  app.Question = Backbone.Model.extend({
+    defaults: {
+      votes: 0,
+      content: ''
+    }
+
+
+  });
+
+});
+
+
+
+
+// ***************************//
+// application views
+// ***************************//
+$(function( $ ) {
+  'use strict';
+
+  app.QuestionView = Backbone.View.extend({
+    //el: '#question-list',
+    tagName: 'div',
+    template: $("#question-template").html(),
+    events: {
+      'click .upvote': 'upvote'
+    },
+
+    initialize: function(){
+      this.render();
+    },
+
+    render: function(){
+      $(this.el).html(Mustache.to_html(this.template, this.model.toJSON()));
+      console.log(this.$el);
+      return this;
+    },
+
+    upvote:function(){
+      var votes = this.$('.votes')[0];
+      var voteTotal = (this.model.get('votes') + 1);
+      this.model.set('votes', voteTotal);
+      votes.innerText = this.model.get('votes');
+      console.log(this.$el);
+    }
+
+  });
+
+  
+  app.AppView = Backbone.View.extend({
+    el: '#question-panel',
+    template: $('#footer-template').html(),
+    events: {
+      'click #ask': 'addQuestion'
+    },
+
+    initialize: function() {
+      console.log('in AppView initialize');
+      this.$footer = this.$("footer");
+      this.$ask = this.$("#ask-input");
+      this.render();
+    },
+
+    render: function(){
+      console.log('in AppView render');
+      this.$footer.html(Mustache.to_html(this.template,{}));
+    },
+
+    addQuestion: function(e) {
+      e.preventDefault();
+      console.log('in AppView addQuestion');
+      console.log( this.$ask.val() );
+      // add view
+      var questionModel = new app.Question({ content: this.$ask.val() });
+      var view = new app.QuestionView({ model: questionModel });
+      this.$("#question-list").append(view.el);
+      this.$ask.val('').focus();
+    }
+
+
+  });
+
+});
+
+
+
+
+
+
+
+// ***************************//
+// initialize application
+// ***************************//
+$(function() {
+  me = new app.AppView();
+});
+  
+
+
+
+var me = {};
