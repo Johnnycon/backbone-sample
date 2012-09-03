5ee8b2f5b03cee8d5ead53053ff52a2aaf9caea4
Cary Landholt <cary@landholt.com>
2012-09-02 22:00:30 -0500

    converted to Angular

--- javascript/app.js
+++ javascript/app.js
@@ -1,133 +1,25 @@
-// load in order of
-// models
-// collections
-// views
-// routers
-// app
+var app = angular.module('app', []);
 
-var jit = jit || {};
-jit.model = jit.model || {};
-jit.collection = jit.collection || {};
-jit.view = jit.view || {};
+app.controller('QuestionsCntl', ['$scope', function ($scope) {
+	var id = -1;
 
-var ENTER_KEY = 13;
+	$scope.questions = [];
 
-// ***************************//
-// applicatoin models
-// ***************************//
+	$scope.ask = function (question) {
+		id += 1;
 
-$(function( $ ){
-  'use strict';
+		var q = {id: id, question: question, votes: 0};
 
-  jit.model.Question = Backbone.Model.extend({
-    defaults: {
-      votes: 0,
-      content: ''
-    }
-  });
+		$scope.questions.push(q);
+	};
 
-  jit.collection.Questions = Backbone.Collection.extend({
-    model: jit.model.Question
-  });
+	$scope.upvote = function (id) {
+		angular.forEach($scope.questions, function (question) {
+			if (angular.equals(question.id, id)) {
+				var votes = question.votes;
 
-});
-
-
-
-
-// ***************************//
-// application views
-// ***************************//
-$(function( $ ) {
-  'use strict';
-
-  jit.view.Question = Backbone.View.extend({
-    //el: '#question-list',
-    tagName: 'div',
-    template: $("#question-template").html(),
-    events: {
-      'click .upvote': 'upvote'
-    },
-
-    initialize: function(){
-      this.render();
-    },
-
-    render: function(){
-      $(this.el).html(Mustache.to_html(this.template, this.model.toJSON()));
-      console.log(this.$el);
-      return this;
-    },
-
-    upvote:function(){
-      var votes = this.$('.votes')[0];
-      var voteTotal = (this.model.get('votes') + 1);
-      this.model.set('votes', voteTotal);
-      votes.innerText = this.model.get('votes');
-      console.log(this.$el);
-    }
-
-  });
-
-  jit.view.App = Backbone.View.extend({
-    el: '#question-panel',
-    template: $('#footer-template').html(),
-    events: {
-      'click #ask': 'addQuestion'
-    },
-
-    initialize: function() {
-      console.log('in AppView initialize');
-      this.$footer = this.$("footer");
-      this.$ask = this.$("#ask-input");
-      this.collection = new jit.collection.Questions();
-      this.collection.bind('add', this.appendItem, this); 
-
-      this.render();
-    },
-
-    render: function(){
-      console.log('in AppView render');
-      this.$footer.html(Mustache.to_html(this.template,{}));
-
-      console.log('AppView render');
-    },
-
-    addQuestion: function(e) {
-      e.preventDefault();
-      console.log('in AppView addQuestion');
-      console.log( this.$ask.val() );
-      // add view
-      var questionModel = new jit.model.Question({ content: this.$ask.val() });
-      this.collection.add({ model: questionModel });
-
-      var view = new jit.view.Question({ model: questionModel });
-      this.$("#question-list").append(view.el);
-      this.$ask.val('').focus();
-    },
-
-    appendItem: function(question){
-      console.log('in AppView appendItem');
-      //var itemView = new ItemView({
-      //  model: item
-      //});
-      //$('ul', this.el).append(itemView.render().el);
-    }    
-
-
-  });
-
-});
-
-
-
-
-
-
-
-// ***************************//
-// initialize application
-// ***************************//
-$(function() {
- new jit.view.App();
-});
+				question.votes = votes + 1;
+			}
+		});
+	};
+}]);
